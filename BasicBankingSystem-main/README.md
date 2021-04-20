5e90595bb60313a9d5c1c6c05328c70737d14a2a
Madhavi0067 <82879834+Madhavi0067@users.noreply.github.com>
2021-04-20 16:58:30 +0530

    Add files via upload

---
+++ BasicBankingSystem-main/README.md
@@ -0,0 +1,2 @@
+# BasicBankingSystem
+Basic Banking System is dynamic website which contain 10 users in which we can transfer amount between them. You can run the project using XAMPP server
---
+++ BasicBankingSystem-main/index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="shortcut icon" href="images/cm.png">
+   
+	 <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("images/11.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+
+	<div id="header">
+       <br>
+       <h1 style=" font-family:Arial; font-size: 50px; color:#00ff00;text-shadow: 2px 2px black;"> Sparks Foundation Internship Project </h1><h4>By Yedla.Madhavi</h4>
+       <h2 style=" font-family:Arial; font-size: 38px;color:#ee10ee;text-shadow: 2px 2px black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Basic Banking System </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="primary_btn" id="button "type="button" href="getdetail.php">View users</button>
+                </a>
+
+               
+                </tr>
+
+                <tr>        
+               <br> <br> <br>
+			   <a href="transfer.php">
+			   <button class="primary_btn" id="button" type="button">Credit Transfer</button>
+               </a>
+               
+               </tr>
+			   
+			    <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="primary_btn" id="button" type="button">All Transaction</button>
+               </a>
+
+            </table>
+    </div>
+	           
+</body>
+</html>
